03099b881286a1755a0ded1013b2ab6c1e0a074d
Kazuo Yamana <k.yamana@gmail.com>
2021-08-05 11:06:38 +0900

    Made sure settings.py can divide local and production.

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.conf import settings
 from django.urls import path, include
 from accounts.views import SignUpView, UserLoginView
 
@@ -9,3 +10,7 @@ urlpatterns = [
     path('accounts/signup', SignUpView.as_view(), name="signup"),
     path('accounts/login', UserLoginView.as_view(), name="login"),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
